c1b29f6a4d08203a4e5f9adae1f5a1af0171e722
Nik Savchenko <nik.savchenko@nielsen.com>
2018-09-09 17:57:45 +0300

    fix bundle github issue

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "docs"),
     filename: "bundle.js",
-    publicPath: "/docs/"
+    publicPath: "./docs/"
   },
   plugins: [
     new webpack.DefinePlugin({
