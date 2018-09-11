36616dc52f86bec04dcb86ff31dc219cac575c16
Nik Savchenko <nik.savchenko@nielsen.com>
2018-09-11 19:52:22 +0300

    add link as a-href on last slide

--- presentation/slides/_990-final.js
+++ presentation/slides/_990-final.js
@@ -4,7 +4,6 @@ import { Slide, Heading, Image } from "spectacle";
 export const notes = `
 Toda raba.
 
-
 Link le page katan im reshimat linkim:
 
 Le React-Redux-Island Repository
@@ -18,14 +17,23 @@ Ve le vdeo aharim shel The Big Web Theory
 Sheelot?
 `;
 
-
 const img = require("../../assets/qr.png");
 
 // noinspection JSUnusedGlobalSymbols
 export default (
   <Slide transition={["fade"]}>
     <Heading size={2}>Thank you</Heading>
-    <Heading size={4}>All links at: https://savcni01.github.io/</Heading>
+    <Heading size={4}>
+      All links at:&nbsp;&nbsp;
+      <a
+        href={"https://savcni01.github.io/"}
+        style={{
+          color: "white"
+        }}
+      >
+        https://savcni01.github.io/
+      </a>
+    </Heading>
     <Image
       bgColor={"white"}
       // width={"760px"}
